768d6de6848dc705adc7f87e7e602ba4a4003d7e
mishlomi <mishlomi@g.jct.ac.il>
2026-03-25 18:17:02 +0200

    בדיקת ההעלאה לגיט

--- ProgramTar0.fs
+++ ProgramTar0.fs
@@ -98,28 +98,6 @@ let main argv =
         // After reading all VM input files, print the final message
         Console.WriteLine($"Output file is ready: {folderName}.asm")
 
-    if File.Exists(inputPath) then
-        // 1. קוראים את כל קובץ ה-VM
-        let lines = File.ReadAllLines(inputPath)
-        
-        // 2. פותחים את ה-CodeWriter שלנו
-        let codeWriter = new CodeWriter(outputPath)
-
-        // 3. עוברים על כל שורה
-        for line in lines do
-            match Parser.parseLine line with
-            | Some(cmd) -> 
-                // אם המפענח מצא פקודה חוקית, שולחים אותה לתרגום
-                match cmd with
-                | Arithmetic(op) -> codeWriter.WriteArithmetic(op)
-                | Push(_, _) | Pop(_, _) -> codeWriter.WritePushPop(cmd)
-                | Unknown -> ()
-            | None -> () // מתעלמים משורות ריקות או הערות
-
-        // 4. סוגרים את הקובץ בסיום
-        codeWriter.Close()
-        printfn "Boom! Translated successfully to %s" outputPath
-    else
-        Console.WriteLine("Error: The specified directory does not exist.")
-
     0
+
+     
